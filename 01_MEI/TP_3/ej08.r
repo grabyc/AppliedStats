d5d6ef4044276907daf5c1423ca6e79a92bf13ee
Gabriel Raby <grabyc@gmail.com>
2022-09-02 20:22:10 -0300

    MEI - TP03 - EJ08 review z-score

--- 01_MEI/TP_3/ej08.r
+++ 01_MEI/TP_3/ej08.r
@@ -25,34 +25,36 @@ sd <- 0.12
 n <- 9
 mean_s <- 20.04
 
-# test
+# rejection zone
 alpha <- 0.05
-std_error <- sd/sqrt(n)
 
-norm_score_inf <- mu + qnorm(alpha/2, 0, 1, lower.tail = T) * std_error 
-norm_score_sup <- mu + qnorm(alpha/2, 0, 1, lower.tail = F) * std_error 
+norm_score_inf <- qnorm(alpha/2, 0, 1, lower.tail = T) 
+norm_score_sup <- qnorm(alpha/2, 0, 1, lower.tail = F) 
 
 # validations
-# pnorm(norm_score_inf, mu, sd, lower.tail = T) ¿<> alpha/2?
+# pnorm(norm_score_inf, 0, 1, lower.tail = T) ¿<> alpha/2?
 # pnorm(norm_score_sup, mu, sd, lower.tail = F) ¿<> alpha/2?
 
 # estimator
-x_obs <- mean_s
+std_error <- sd/sqrt(n)
+z_obs <- (mean_s - mu) / std_error
 
 # decision rule
-#  -  x_obs <= norm_score_inf || x_obs >= norm_score_sup  => reject H_0
-#  -  norm_score_inf < x_obs < norm_score_sup  => don't reject H_0
-x_obs <= norm_score_inf || x_obs >= norm_score_sup
+#  -  z_obs <= norm_score_inf || z_obs >= norm_score_sup  => reject H_0
+#  -  norm_score_inf < z_obs < norm_score_sup  => don't reject H_0
+z_obs <= norm_score_inf || z_obs >= norm_score_sup
 
 # decision rule
 #  -  p_value < alpha  => reject H_0
-p_value <- pnorm(x_obs, mu, sd, lower.tail = F) 
+p_value <- pnorm(z_obs, 0, 1, lower.tail = F) 
 
 p_value < alpha
 
 # graphical representation
-x_inf <- 19.5
-x_sup <- 20.5
+x_inf <- -3
+x_sup <- 3
+mu <- 0
+sd <- 1
 events <- seq(x_inf,x_sup,0.05)
 df <- data.frame(events, density = dnorm(x = events, mu, sd))  
 
@@ -87,18 +89,18 @@ ggplot(df, aes(x = events)) +
                 geom = "area",
                 alpha = 0.6,
                 fill = "steelblue",
-                xlim = c(x_obs,x_sup)) + 
-  geom_segment(aes(x = x_obs, xend = x_obs, y = 0, yend = dnorm(x_obs, mu, sd)), 
+                xlim = c(z_obs,x_sup)) + 
+  geom_segment(aes(x = z_obs, xend = z_obs, y = 0, yend = dnorm(z_obs, mu, sd)), 
                lty = "dashed") + 
   geom_text(
-    aes(label = paste(c("P-value = ", round(p_value,5)), collapse = ""), x = x_obs, y = dnorm(x_obs, mu, sd) + 0.01),
+    aes(label = paste(c("P-value = ", round(p_value,5)), collapse = ""), x = z_obs, y = dnorm(z_obs, mu, sd) + 0.01),
     position = position_dodge(0.9),
     size = 3,
     vjust = 0
   ) +
   labs(title = "Hypothesis: mu = 20",
        subtitle = "alpha = 5%",
-       x = "X",
+       x = "z_score",
        y = "Density")
 
 
