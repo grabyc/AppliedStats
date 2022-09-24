7ac3e57dd491a5bdcbae07bff970194238cf4f54
Gabriel Raby <grabyc@gmail.com>
2022-09-24 17:52:42 -0300

    MEI - TP03 - EJ08 review type II error

--- 01_MEI/TP_3/ej08.r
+++ 01_MEI/TP_3/ej08.r
@@ -105,10 +105,101 @@ ggplot(df, aes(x = events)) +
 
 
 
-# c) mu_real = 19.95mm ; prob de aceptar H_0?
+# c) mu_real = 19.95mm, mismo desvio ; prob de aceptar H_0? (error tipo II = beta)
 
 # H_0 (está bien ajustado): mu = 20mm 
 # H_a (posición incorrecta): mu = 19.95mm
 # muestra aleatoria de 9 piezas x_muestral = 20.04mm
 
+# population
+# H_0  - normal distrib
+mu <- 20
+sd <- 0.12
+
+# real  - normal distrib
+mu_real <- 19.95
+sd <- 0.12
+
+# sample
+n <- 9
+std_error <- sd/sqrt(n)
+
+# rejection zone
+alpha <- 0.05
+
+rej_inf <- qnorm(alpha/2, mu, std_error, lower.tail = T) 
+rej_sup <- qnorm(alpha/2, mu, std_error, lower.tail = F) 
+
+# type II error - beta
+beta <- pnorm(rej_inf, mu_real, std_error, lower.tail = F)
+
+# graphical representation
+x_inf <- mu - std_error * 4
+x_sup <- mu + std_error * 4
+events <- seq(x_inf,x_sup,0.01)
+df <- data.frame(events, density = dnorm(x = events, mu, std_error))  
+
+ggplot(df, aes(x = events)) +
+  stat_function(fun = dnorm,
+                args = list(
+                  mean = mu,
+                  sd = std_error
+                ),
+                geom = "line") +
+  stat_function(fun = dnorm,
+                args = list(
+                  mean = mu,
+                  sd = std_error
+                ),
+                geom = "area",
+                alpha = 0.8,
+                xlim = c(rej_sup,x_sup)) + 
+  geom_segment(aes(x = rej_sup, xend = rej_sup, y = 0, yend = dnorm(rej_sup, mu, std_error)), 
+               lty = "dashed") + 
+  geom_text(
+    aes(label = paste(c("alpha/2 = ", round(rej_sup,5)), collapse = ""), x = rej_sup, y = dnorm(rej_sup, mu, std_error) + 0.01),
+    size = 3,
+    vjust = 0
+  ) +
+  stat_function(fun = dnorm,
+                args = list(
+                  mean = mu,
+                  sd = std_error
+                ),
+                geom = "area",
+                alpha = 0.8,
+                xlim = c(x_inf,rej_inf)) +
+  geom_segment(aes(x = rej_inf, xend = rej_inf, y = 0, yend = dnorm(rej_inf, mu, std_error)), 
+               lty = "dashed") + 
+  geom_text(
+    aes(label = paste(c("-alpha/2 = ", round(rej_inf,5)), collapse = ""), x = rej_inf, y = dnorm(rej_inf, mu, std_error) + 0.01),
+    size = 3,
+    vjust = 0
+  ) +
+  stat_function(fun = dnorm,
+                args = list(
+                  mean = mu_real,
+                  sd = std_error
+                ),
+                geom = "line") +
+  stat_function(fun = dnorm,
+                args = list(
+                  mean = mu_real,
+                  sd = std_error
+                ),
+                geom = "area",
+                alpha = 0.6,
+                fill = "steelblue",
+                xlim = c(rej_inf,x_sup)) + 
+  geom_text(
+    aes(label = paste(c("beta = ", round(beta,5)), collapse = ""), x = mu_real, y = dnorm(mu_real, mu_real, std_error) / 2),
+    size = 3,
+    vjust = 0
+  ) +
+  labs(title = "Hypothesis: mu = 20 (real mu = 19.95)",
+       subtitle = "alpha = 5%",
+       x = "Long (mm)",
+       y = "Density")
+
 # e) potencia de la prueba
+power <- 1- beta
